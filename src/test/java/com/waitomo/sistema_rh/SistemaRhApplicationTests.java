27ddff47ec51478445c8d5f48d123a9e44dd7b0c
Namibo06 <namibo654@gmail.com>
2024-06-28 13:26:25 -0300

    feat: tests

--- src/test/java/com/waitomo/sistema_rh/SistemaRhApplicationTests.java
+++ src/test/java/com/waitomo/sistema_rh/SistemaRhApplicationTests.java
@@ -1,13 +1,31 @@
 package com.waitomo.sistema_rh;
 
+import com.waitomo.sistema_rh.dtos.EnterpriseDTO;
+import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
+import com.waitomo.sistema_rh.services.EnterpriseService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SistemaRhApplicationTests {
+	@Autowired
+	private EnterpriseService enterpriseService;
 
 	@Test
-	void contextLoads() {
+	public void testCreateEnterprise(){
+		EnterpriseDTO dto = new EnterpriseDTO(null, "12345678912345", "nome ficticio", "nome empresarial", null);
+		ResponseMessageStatus result = enterpriseService.createEnterpriseService(dto);
+		assertNotNull(result);
+	}
+
+	@Test
+	public void testCnpjLength(){
+		EnterpriseDTO dto = new EnterpriseDTO(null, "1234567891245", "nome ficticio", "nome empresarial", null);
+		assertEquals(dto.cnpj().length(),14);
 	}
 
 }
