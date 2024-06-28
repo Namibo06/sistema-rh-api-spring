0835022c98a24d92719a4fe83c3e28b725926a9c
Namibo06 <namibo654@gmail.com>
2024-06-28 00:26:23 -0300

    feat: Config ModelMapper

---
+++ src/main/java/com/waitomo/sistema_rh/configs/Config.java
@@ -0,0 +1,13 @@
+package com.waitomo.sistema_rh.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class Config {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
