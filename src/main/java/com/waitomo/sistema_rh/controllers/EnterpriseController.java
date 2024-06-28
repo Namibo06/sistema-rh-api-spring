46a58d3c994f9928db3fccfdbdd14e179a092f21
Namibo06 <namibo654@gmail.com>
2024-06-28 00:46:40 -0300

    feat: createEnterprise

---
+++ src/main/java/com/waitomo/sistema_rh/controllers/EnterpriseController.java
@@ -0,0 +1,36 @@
+package com.waitomo.sistema_rh.controllers;
+
+import com.waitomo.sistema_rh.dtos.EnterpriseDTO;
+import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
+import com.waitomo.sistema_rh.services.EnterpriseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(name = "enterprises")
+public class EnterpriseController {
+    @Autowired
+    private EnterpriseService enterpriseService;
+
+    @PostMapping
+    public ResponseEntity<ResponseMessageStatus> createEnterprise(@RequestBody EnterpriseDTO enterprise, UriComponentsBuilder uriBuilder){
+        EnterpriseDTO enterpriseDTO=enterpriseService.createEnterpriseService(enterprise);
+        URI path = uriBuilder.path("/enterprises/{id}").buildAndExpand(enterprise.id()).toUri();
+
+        String message="Empresa criada com sucesso!";
+        Integer status = 201;
+        ResponseMessageStatus responseMessageStatus = new ResponseMessageStatus(
+                message,
+                status
+        );
+
+        return ResponseEntity.created(path).body(responseMessageStatus);
+    }
+}
