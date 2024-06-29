ae1875eca58d56c325b1e2178d7c30be14298e4d
Namibo06 <namibo654@gmail.com>
2024-06-29 00:34:07 -0300

    refactor: Point

--- src/main/java/com/waitomo/sistema_rh/models/Point.java
+++ src/main/java/com/waitomo/sistema_rh/models/Point.java
@@ -1,7 +1,30 @@
 package com.waitomo.sistema_rh.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "point")
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="employee_id")
+    private Long employeeId;
+    @Column(nullable = false)
+    private LocalDate date;
+    private LocalTime chekInTime;
+    private LocalTime checkOutLunch;
+    private LocalTime backLunch;
+    private LocalTime CheckOutTime;
 }
