5da26aebb391a4b2a2fe19779fd94a80dbd86999
Namibo06 <namibo654@gmail.com>
2024-06-28 23:47:32 -0300

    fix: UserLevelDTO

--- src/main/java/com/waitomo/sistema_rh/dtos/UserLevelDTO.java
+++ src/main/java/com/waitomo/sistema_rh/dtos/UserLevelDTO.java
@@ -1,4 +1,8 @@
 package com.waitomo.sistema_rh.dtos;
 
-public record UserLevelDTO(String name,Long enterprise_id) {
+public record UserLevelDTO(
+        Long id,
+        String name,
+        Long enterprise_id
+) {
 }
