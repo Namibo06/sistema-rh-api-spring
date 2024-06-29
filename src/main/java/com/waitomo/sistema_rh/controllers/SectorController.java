a6302d8225e7d7c67bf14450ba5bb3d0fd8ca1a8
Namibo06 <namibo654@gmail.com>
2024-06-29 00:40:06 -0300

    refactor: SectorController

--- src/main/java/com/waitomo/sistema_rh/controllers/SectorController.java
+++ src/main/java/com/waitomo/sistema_rh/controllers/SectorController.java
@@ -6,16 +6,21 @@ import com.waitomo.sistema_rh.services.SectorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping("/sectors")
+@RequestMapping("/sector")
 public class SectorController {
     @Autowired
     private SectorService service;
 
     @PostMapping
-    public ResponseEntity<ResponseMessageStatus> createSector(@RequestBody SectorDTO sector){
+    public ResponseEntity<ResponseMessageStatus> createSector(@RequestBody SectorDTO sector, UriComponentsBuilder uriBuilder){
         ResponseMessageStatus response = service.createSector(sector);
-        return ResponseEntity.ok(response);
+        URI path = uriBuilder.path("sector/{id}").buildAndExpand(sector.id()).toUri();
+
+        return ResponseEntity.created(path).body(response);
     }
 }
