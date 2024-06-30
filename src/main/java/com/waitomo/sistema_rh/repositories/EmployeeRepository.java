bf08c7345814e95c4a51146c86b10e3660e6d530
Namibo06 <namibo654@gmail.com>
2024-06-30 01:54:08 -0300

    refactor: EmployeeRepository

--- src/main/java/com/waitomo/sistema_rh/repositories/EmployeeRepository.java
+++ src/main/java/com/waitomo/sistema_rh/repositories/EmployeeRepository.java
@@ -2,9 +2,17 @@ package com.waitomo.sistema_rh.repositories;
 
 import com.waitomo.sistema_rh.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    //UserDetails findByLogin(String login);
 
+    boolean existsByLoginAndPassword(String login,String password);
+
+    Optional<Employee> findByLogin(String login);
 }
