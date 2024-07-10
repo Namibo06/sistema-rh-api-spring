82b8c7480fdb7c600c215f1d4e00db99df3f74f3
Namibo06 <namibo654@gmail.com>
2024-07-10 17:56:05 -0300

    fix: updateNumberEmployee

--- src/main/java/com/waitomo/sistema_rh/services/EmployeeService.java
+++ src/main/java/com/waitomo/sistema_rh/services/EmployeeService.java
@@ -6,6 +6,7 @@ import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
 import com.waitomo.sistema_rh.models.Employee;
 import com.waitomo.sistema_rh.models.Enterprise;
 import com.waitomo.sistema_rh.repositories.EmployeeRepository;
+import com.waitomo.sistema_rh.repositories.EnterpriseRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +18,7 @@ public class EmployeeService {
     private EmployeeRepository repository;
 
     @Autowired
-    private EnterpriseService enterpriseService;
+    private EnterpriseRepository enterpriseRepository;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -38,13 +39,21 @@ public class EmployeeService {
         employeeModel.setLogin(employeeDTO.login());
         employeeModel.setPassword(encoder.encode(employeeDTO.password()));
         employeeModel.setToken(employeeDTO.token());
-
         repository.save(employeeModel);
 
-        enterpriseService.updateNumberEmployee(employeeDTO.cnpjEnterprise());
+        updateNumberEmployee(employeeDTO.cnpjEnterprise());
 
         String message="Funcionário criado com sucesso";
         Integer status=201;
         return new ResponseMessageStatus(message,status);
+    }
+
+    public void updateNumberEmployee(String cnpj){
+        Enterprise enterprise= enterpriseRepository.findByCnpj(cnpj);
+        if(enterprise.getNumber_employees() == null){
+            enterprise.setNumber_employees(0);
+        }
+        enterprise.setNumber_employees(enterprise.getNumber_employees() + 1);
+        enterpriseRepository.save(enterprise);
     }
 }
