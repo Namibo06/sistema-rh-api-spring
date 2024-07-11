58aa1eec9eaa186916cf4782b4ec87016bad4248
Namibo06 <namibo654@gmail.com>
2024-07-10 22:27:21 -0300

    refactor: getAllSector

--- src/main/java/com/waitomo/sistema_rh/controllers/SectorController.java
+++ src/main/java/com/waitomo/sistema_rh/controllers/SectorController.java
@@ -4,6 +4,9 @@ import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
 import com.waitomo.sistema_rh.dtos.SectorDTO;
 import com.waitomo.sistema_rh.services.SectorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,8 +22,15 @@ public class SectorController {
     @PostMapping
     public ResponseEntity<ResponseMessageStatus> createSector(@RequestBody SectorDTO sector, UriComponentsBuilder uriBuilder){
         ResponseMessageStatus response = service.createSector(sector);
-        URI path = uriBuilder.path("sector/{id}").buildAndExpand(sector.id()).toUri();
+        URI path = uriBuilder.path("sector/{id}").buildAndExpand(sector.getId()).toUri();
 
         return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<SectorDTO>> getAllSector(@PageableDefault(size = 15) Pageable pageable){
+        Page<SectorDTO> response = service.getAllSectorService(pageable);
+
+        return ResponseEntity.ok(response);
     }
 }
