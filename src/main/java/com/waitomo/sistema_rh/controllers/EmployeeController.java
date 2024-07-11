4d77cf851110aad09ffa03a9d7b1e8e0332618e5
Namibo06 <namibo654@gmail.com>
2024-07-10 23:28:15 -0300

    feat: getAllEmployee

--- src/main/java/com/waitomo/sistema_rh/controllers/EmployeeController.java
+++ src/main/java/com/waitomo/sistema_rh/controllers/EmployeeController.java
@@ -4,11 +4,11 @@ import com.waitomo.sistema_rh.dtos.EmployeeDTO;
 import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
 import com.waitomo.sistema_rh.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -22,7 +22,14 @@ public class EmployeeController {
     @PostMapping
     public ResponseEntity<ResponseMessageStatus> createEmployee(@RequestBody EmployeeDTO employee, UriComponentsBuilder uriBuilder){
        ResponseMessageStatus response = service.createEmployeeService(employee);
-       URI path = uriBuilder.path("/employee/{id}").buildAndExpand(employee.id()).toUri();
+       URI path = uriBuilder.path("/employee/{id}").buildAndExpand(employee.getId()).toUri();
        return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeDTO>> getAllEmployee(@PageableDefault(size = 15) Pageable pageable){
+        Page<EmployeeDTO> response = service.getAllEmployeeService(pageable);
+
+        return ResponseEntity.ok(response);
     }
 }
