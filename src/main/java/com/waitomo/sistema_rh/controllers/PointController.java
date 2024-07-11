4f1e812100b6b6423dea2225bbef7fff297a25e1
Namibo06 <namibo654@gmail.com>
2024-07-11 10:24:03 -0300

    feat: getAllPoint

--- src/main/java/com/waitomo/sistema_rh/controllers/PointController.java
+++ src/main/java/com/waitomo/sistema_rh/controllers/PointController.java
@@ -4,11 +4,11 @@ import com.waitomo.sistema_rh.dtos.PointDTO;
 import com.waitomo.sistema_rh.dtos.ResponseMessageStatus;
 import com.waitomo.sistema_rh.services.PointService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -25,5 +25,12 @@ public class PointController {
         URI path = uriBuilder.path("/point/{id}").buildAndExpand(pointDTO.id()).toUri();
 
         return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PointDTO>> getAllPoint(@PageableDefault(size = 15) Pageable pageable){
+        Page<PointDTO> response = service.getAllPointService(pageable);
+
+        return ResponseEntity.ok(response);
     }
 }
