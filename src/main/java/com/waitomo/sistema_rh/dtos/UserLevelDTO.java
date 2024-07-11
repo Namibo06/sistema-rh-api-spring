cfe71665ea4782a5e8dfefd746a83f2fa99467a5
Namibo06 <namibo654@gmail.com>
2024-07-11 16:26:04 -0300

    refactor: UserLevelDTO

--- src/main/java/com/waitomo/sistema_rh/dtos/UserLevelDTO.java
+++ src/main/java/com/waitomo/sistema_rh/dtos/UserLevelDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserLevelDTO {
-    Long id;
-    String name;
-    Long enterprise_id;
+    private Long id;
+    private String name;
+    private Long enterprise_id;
 }
