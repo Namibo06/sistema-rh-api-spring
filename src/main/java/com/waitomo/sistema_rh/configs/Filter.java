81379a4da71491519c63642aa96ee32bf59e61fd
Namibo06 <namibo654@gmail.com>
2024-07-23 23:31:50 -0300

    feat: Filter

---
+++ src/main/java/com/waitomo/sistema_rh/configs/Filter.java
@@ -0,0 +1,54 @@
+package com.waitomo.sistema_rh.configs;
+
+import com.waitomo.sistema_rh.exceptions.NotFoundException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class Filter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        if("/employee".equals(requestURI) || "/login".equals(requestURI) || "/enterprise".equals(requestURI) || requestURI.startsWith("/swagger-ui/") || requestURI.startsWith("/v3/api-docs") || requestURI.startsWith("favicon.ico")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        try{
+            var token = findToken(request);
+
+            if (token == null) {
+                throw new NotFoundException("Token",'o');
+            }
+
+            filterChain.doFilter(request,response);
+        }catch (NotFoundException ex){
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+            response.setContentType("application/json");
+            PrintWriter writer = response.getWriter();
+            writer.write("{\"message\": \"" + ex.getMessage() + "\", \"status\": " + HttpStatus.NOT_FOUND.value() + "}");
+            writer.flush();
+        }
+
+
+    }
+
+    private String findToken(HttpServletRequest request){
+        var authorization = request.getHeader("Authorization");
+        if(authorization == null){
+            throw new NotFoundException("Token",'o');
+        }
+
+        return authorization.replace("Bearer","");
+    }
+}
