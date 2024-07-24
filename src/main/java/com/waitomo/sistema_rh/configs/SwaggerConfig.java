dbdeec359123a007174affb80f5a38f28a9408f3
Namibo06 <namibo654@gmail.com>
2024-07-23 23:35:31 -0300

    refactor: add security

--- src/main/java/com/waitomo/sistema_rh/configs/SwaggerConfig.java
+++ src/main/java/com/waitomo/sistema_rh/configs/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.waitomo.sistema_rh.configs;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Sistema RH",description = "API RESTFull de um Sistema para RH",version = "1.0.0")
+        info = @Info(title = "Sistema RH",description = "API RESTFull de um Sistema para RH",version = "1.0.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
+@SecurityScheme(name = "bearerAuth",type = SecuritySchemeType.HTTP,scheme = "bearer",bearerFormat = "JWT")
 public class SwaggerConfig {
 }
